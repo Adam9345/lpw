17c501b861e2f51070bb9e9448f7e8b6fb4549f5
Adam9345 <klonica3a@gmail.com>
2018-09-28 18:08:34 +0200

    Added chartjs component

---
+++ src/components/ChartJS/index.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { Bar, HorizontalBar, Pie } from "react-chartjs-2";
+import { dataBar, dataPie, options } from "./chart-options";
+
+class ChartJS extends React.Component {
+  public render() {
+    return (
+      <div>
+        <Bar data={dataBar} width={800} height={300} options={options} />
+        <HorizontalBar
+          data={dataBar}
+          width={800}
+          height={300}
+          options={options}
+        />
+        <Pie data={dataPie} width={800} height={300} />
+      </div>
+    );
+  }
+}
+
+export default ChartJS;
