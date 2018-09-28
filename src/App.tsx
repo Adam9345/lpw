808eb6e46bce38d3df323ee06e177e295a909f4d
Adam9345 <klonica3a@gmail.com>
2018-09-28 18:06:32 +0200

    Changed urls and added new component chartjs

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,25 @@ import "popper.js/dist/popper.min.js";
 import * as React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ChartGroup from "src/components/ChartGroup";
+import ChartJS from "src/components/ChartJS";
 import "./App.css";
 import Header from "./components/Header";
 
 class App extends React.Component {
   public render() {
     const items = [
-      { name: "Home", url: "/lpw/" },
-      { name: "Link", url: "/lpw/link" },
-      { name: "Menu3", url: "#menu3" }
+      { name: "CanvasJS", url: "/lpw/" },
+      { name: "ChartJS", url: "/lpw/chartjs" }
     ];
 
     return (
       <Router>
         <div>
-          <Header theme="dark" items={items} title="Wykresy" />
+          <Header theme="dark" items={items} title="Charts" />
           <Switch>
             <Route exact={true} path="/" component={ChartGroup} />
             <Route exact={true} path="/lpw/" component={ChartGroup} />
-            <Route path="/lpw/link" component={ChartGroup} />
+            <Route path="/lpw/chartjs" component={ChartJS} />
           </Switch>
         </div>
       </Router>
