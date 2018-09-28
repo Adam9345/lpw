d7370063f41af3aa02a9092721c9f10d9bf09715
Adam9345 <klonica3a@gmail.com>
2018-09-28 15:41:01 +0200

    Added some options for charts

---
+++ src/components/ChartGroup/chart-options.ts
@@ -0,0 +1,125 @@
+const animationEnabled = true;
+const theme = "dark1";
+
+export const charts = [
+  {
+    animationEnabled,
+    data: [
+      {
+        // Change type to "doughnut", "line", "splineArea", etc.
+        dataPoints: [
+          { label: "apple", y: 10 },
+          { label: "orange", y: 15 },
+          { label: "banana", y: 25 },
+          { label: "mango", y: 30 },
+          { label: "grape", y: 28 }
+        ],
+        type: "column"
+      }
+    ],
+    theme,
+    title: {
+      text: "My First Chart in CanvasJS"
+    }
+  },
+  {
+    animationEnabled,
+    data: [
+      {
+        // Change type to "doughnut", "line", "splineArea", etc.
+        dataPoints: [
+          { label: "apple", y: 10 },
+          { label: "orange", y: 15 },
+          { label: "banana", y: 25 },
+          { label: "mango", y: 30 },
+          { label: "grape", y: 28 }
+        ],
+        type: "pie"
+      }
+    ],
+    theme,
+    title: {
+      text: "My First Chart in CanvasJS"
+    }
+  },
+  {
+    animationEnabled,
+    data: [
+      {
+        // Change type to "doughnut", "line", "splineArea", etc.
+        dataPoints: [
+          { label: "apple", y: 10 },
+          { label: "orange", y: 15 },
+          { label: "banana", y: 25 },
+          { label: "mango", y: 30 },
+          { label: "grape", y: 28 }
+        ],
+        type: "pyramid"
+      }
+    ],
+    theme,
+    title: {
+      text: "My First Chart in CanvasJS"
+    }
+  },
+  {
+    animationEnabled,
+    data: [
+      {
+        // Change type to "doughnut", "line", "splineArea", etc.
+        dataPoints: [
+          { label: "apple", y: 10 },
+          { label: "orange", y: 15 },
+          { label: "banana", y: 25 },
+          { label: "mango", y: 30 },
+          { label: "grape", y: 28 }
+        ],
+        type: "doughnut"
+      }
+    ],
+    theme,
+    title: {
+      text: "My First Chart in CanvasJS"
+    }
+  },
+  {
+    animationEnabled,
+    data: [
+      {
+        // Change type to "doughnut", "line", "splineArea", etc.
+        dataPoints: [
+          { label: "apple", y: 10 },
+          { label: "orange", y: 15 },
+          { label: "banana", y: 25 },
+          { label: "mango", y: 30 },
+          { label: "grape", y: 28 }
+        ],
+        type: "line"
+      }
+    ],
+    theme: "light2",
+    title: {
+      text: "My First Chart in CanvasJS"
+    }
+  },
+  {
+    animationEnabled,
+    data: [
+      {
+        // Change type to "doughnut", "line", "splineArea", etc.
+        dataPoints: [
+          { label: "apple", y: 10 },
+          { label: "orange", y: 15 },
+          { label: "banana", y: 25 },
+          { label: "mango", y: 30 },
+          { label: "grape", y: 28 }
+        ],
+        type: "bar"
+      }
+    ],
+    theme,
+    title: {
+      text: "My First Chart in CanvasJS"
+    }
+  }
+];
