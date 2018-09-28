fd7cdffa1d5e8140744a47ddce79c52d5c1f4cea
Adam9345 <klonica3a@gmail.com>
2018-09-28 15:43:58 +0200

    Added component header and chartgroup

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import * as React from 'react';
-import './App.css';
-
-import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min";
+import "jquery/dist/jquery.slim.min";
+import "popper.js/dist/popper.min.js";
+import * as React from "react";
+import ChartGroup from "src/components/ChartGroup";
+import "./App.css";
+import Header from "./components/Header";
 
 class App extends React.Component {
   public render() {
+    const items = [
+      { name: "Home", url: "#home" },
+      { name: "Link", url: "#link" },
+      { name: "Menu3", url: "#menu3" }
+    ];
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div>
+        <Header theme="dark" items={items} title="Wykresy" />
+        <ChartGroup />
       </div>
     );
   }
